3662077c760ee8d49684601014e4a2e8b543a714
tsegay <tsegay.berhanu186@gmail.com>
2024-10-28 15:35:22 +0300

    enable: cors

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import corsOptions from "./config/corsoption.js";
 
-const app = express()
+import verifySignature from "./middleware/verifySignature.js";
+import preventReplayAttack from "./middleware/preventReplayAttack.js";
+import signRequest from "./helpers/signRequest.js";
+const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+app.post("/webhook", signRequest, verifySignature, preventReplayAttack,   (req, res) => {
+  res.status(200).send("Webhook received successfully");
+  console.log(req.headers)
+
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
