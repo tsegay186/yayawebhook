e6b06d3620e5db0715ae3cbe3e3082dba6a7f40e
tsegay <tsegay.berhanu186@gmail.com>
2024-10-28 14:22:16 +0300

    sign payload for signature validation

---
+++ helpers/signPayload.js
@@ -0,0 +1,12 @@
+import crypto from 'crypto'
+
+const SECRET_KEY = process.env.SECRET_KEY || "key";
+const sign = (payload)=> {
+    const hmac = crypto.createHmac("sha256", SECRET_KEY);
+    const signedPayload = Object.values(payload).join("");
+    const encodedPayload = Buffer.from(signedPayload, 'utf-8');
+    const result = hmac.update(encodedPayload).digest('base64');
+    return result
+  }
+
+  export default sign
